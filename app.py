aa54cf59ffbcb5f72e7cbe455a515ae0c26630ca
najib <muhtasimmc@gmail.com>
2018-03-16 22:52:43 -0400

    fully function!!! yayyy, it can send and recieve messages seamlessly, also added a view channels button to go back from chat page to channels page, as of right now i need to include an extra feature

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template, request, jsonify, session
+from flask import Flask, render_template, request, jsonify, session, redirect, url_for
 from flask_socketio import SocketIO, emit
 from flask_session import Session
 
@@ -25,67 +25,88 @@ server = dict()
 
 
 @app.route("/")
-def index():
-
-	return render_template('index.html')
-
-
-@app.route("/channels", methods=["POST"])
-def channels():
-	name = request.form.get('user')
-	if name:
-		# user enters for first time
-		# check if user list exists
+def index():	
+	# if session['current_channel']:
+	# 	return redirect(url_for('channel', channel=session['current_channel']) )
+	# else:
+	# 	return render_template('index.html')
+	if 'channels' in server: 
 		try:
-			# add user if user does not exist in list
-			if not name in server['users']:
-				server['users'].append(name)
+			return redirect(url_for('channel', channel=session['current_channel']) )
 		except KeyError:
-			# if list doesn't exist, create and add user
-			server['users'] = []
-			server['users'].append(name)
+			return render_template('index.html')
 		except:
-			return "you dun goofed"
-
-		# log user in
-		session['current_user'] = name
-		# return render_template('channels.html', cur=session['current_user'])
-
+			return 'you dun goofed'
 	else:
-
-		# create a channel
-		try:
-			server['channels']
-		except:
-			server['channels'] = []
+		return render_template('index.html')
 
 
-		if len(server['channels']) == 0:
-			# create channel list of dicts
-			server['channel_count'] = 0
-			count = server['channel_count']
-			server['channels'].append({ 'name': str(count) , 'messages': []})
-			# return render_template('channels.html', channels=session['channels'])
+
+
+@app.route("/channels", methods=["POST", "GET"])
+def channels():
+	# coming from channel page
+	if request.method == 'GET':
+		return render_template('channels.html', channels=server['channels'], cur=session['current_user'])
+	else:
+		name = request.form.get('user')
+		if name:
+			# user enters for first time
+			# check if user list exists
+			try:
+				# add user if user does not exist in list
+				if not name in server['users']:
+					server['users'].append(name)
+			except KeyError:
+				# if list doesn't exist, create and add user
+				server['users'] = []
+				server['users'].append(name)
+			except:
+				return "you dun goofed"
+
+			# log user in
+			session['current_user'] = name
+			# return render_template('channels.html', cur=session['current_user'])
 
 		else:
-			# list already exists so just create add another channel
-			server['channel_count'] += 1 
-			count = server['channel_count']
-			server['channels'].append({ 'name': str(count) , 'messages': []})
+
+			# create a channel
+			try:
+				server['channels']
+			except:
+				server['channels'] = []
 
 
-	try:
-		return  render_template('channels.html', channels=server['channels'], cur=session['current_user'])
-	except KeyError:
-		return  render_template('channels.html', cur=session['current_user'])
-		# jsonify(session['channels'])
+			if len(server['channels']) == 0:
+				# create channel list of dicts
+				server['channel_count'] = 0
+				count = server['channel_count']
+				server['channels'].append({ 'name': str(count) , 'messages': []})
+				
+			else:
+				# list already exists so just create add another channel
+				server['channel_count'] += 1 
+				count = server['channel_count']
+				server['channels'].append({ 'name': str(count) , 'messages': []})
+
+
+		try:
+			return  render_template('channels.html', channels=server['channels'], cur=session['current_user'])
+		except KeyError:
+			return  render_template('channels.html', cur=session['current_user'])
+			# jsonify(session['channels'])
 
 
 
 @app.route("/channels/<string:channel>")
 def channel(channel):
-
+	# get channel id
 	channel_num = int(channel)
+
+	# remember channel id as a cookie
+	session['current_channel'] = channel_num
+
+
 	# list of messages 
 	messages = server['channels'][channel_num]['messages']
 
@@ -102,7 +123,11 @@ def message(info):
 	time = info['time']
 	
 	# add message to messages list to the CORRECT CHANNEL
-	server['channels'][channel_num]['messages'].append( user + ": " + message +" Time: " + time)
+	# server['channels'][channel_num]['messages'].append( user + ": " + message +" Time: " + time)
 
-
+	server['channels'][channel_num]['messages'].append( {'user': user, 'message': message, 'time': time})
 	emit('display message', {"new": message, 'user': user, 'channel_num': channel_num, 'time': time}, broadcast=True)
+
+@app.route("/fuck")
+def fuck():
+	return str(session['current_channel'])
